8466570d7725db7be1e1d1ec91ccb9db9fa78737
Geoff McLane <ubuntu@geoffair.info>
2017-12-11 20:03:59 +0100

    Add func to header

--- src/Utf8Ucs2Converter.h
+++ src/Utf8Ucs2Converter.h
@@ -2,6 +2,8 @@
 #ifndef _UTF8UCS2CONVERT_H_
 #define _UTF8UCS2CONVERT_H_
 
+extern std::wstring utf8ToUcs2(const std::string& utf8Str);
+extern std::string ucs2ToUtf8(const std::wstring& ucs2Str);
 
 #endif /* _UTF8UCS2CONVERT_H_ */
 /* eof */
