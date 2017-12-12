5489b2dc6e08fd1f39f368d4110b45a73a1a046f
Geoff McLane <ubuntu@geoffair.info>
2017-12-12 03:34:58 +0100

    Add --reverse option UCS2 to UTF8

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@
 #include <stdio.h>
 #include <string.h> // for strdup(), ...
 #include <string>
+#include <algorithm> // for find_if
+#include <cctype> // for isspace
 #include <stdint.h>
 #include "Utf8Ucs2Converter.h"
 
@@ -21,14 +23,18 @@ static const char *module = "main";
 
 static const char *usr_input = 0;
 static const char *out_file = "tempout.txt";
-
+static int reverse = 0;
 void give_help( char *name )
 {
     printf("%s: usage: [options] usr_input\n", module);
     printf("Options:\n");
     printf(" --help  (-h or -?) = This help and exit(0)\n");
     printf(" --out <file>  (-o) = Set output file. (def=%s)\n", out_file);
-    // TODO: More help
+    printf(" --reverse     (-r) = Read in unicode, and output utf-8\n");
+    printf("\n");
+    printf(" Read user input file as utf-8, and output UCS-2 to output file.\n");
+    printf(" And vice versa if '--reverse', (-r), given...\n");
+    printf(" Exit(0) if help shown, or success. Exit(1) otherwise.\n");
 }
 
 int parse_args( int argc, char **argv )
@@ -60,6 +66,9 @@ int parse_args( int argc, char **argv )
                     return 1;
                 }
                 break;
+            case 'r':
+                reverse = 1;
+                break;
             // TODO: Other arguments
             default:
                 printf("%s: Unknown argument '%s'. Try -? for help...\n", module, arg);
@@ -81,6 +90,7 @@ int parse_args( int argc, char **argv )
     return 0;
 }
 
+#if 0 // 00000000000000000000000000000000000000000
 int add_BOM_utf16(std::wofstream &fs)
 {
     unsigned char smarker[2];
@@ -99,8 +109,9 @@ int add_BOM_utf8(std::ofstream &fs)
     fs << smarker;
     return 0;
 }
+#endif // 0000000000000000000000000000000000000000
 
-int process_in_file(const char *file) // actions of app
+int process_in_file_UTF8(const char *file) // actions of app
 {
     int iret = 0;
     int lines = 0;
@@ -149,6 +160,93 @@ int process_in_file(const char *file) // actions of app
     return iret;
 }
 
+// trim from end (in place) - THIS FAILED
+#if 0 // 0000000000000000000000000000000000000000000000
+static inline void rtrim(std::wstring &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+#endif // 000000000000000000000000000000000000000000000
+
+static void trim_wCR(std::wstring &mystring)
+{
+    if (!mystring.empty()) {
+        size_t len = mystring.size();
+        if (len > 1) {
+            int ch = mystring[len - 2];
+            if (ch == '\r')
+                mystring.erase(len - 2);
+        }
+    }
+}
+
+int process_in_file_UCS2(const char *file) // actions of app
+{
+    int iret = 0;
+    int lines = 0;
+    size_t out_len = 0;
+    size_t len;
+    std::wstring str, str2;
+    std::wifstream ifs(file);
+    if (!ifs) {
+        std::cerr << "Error: FAILED to open '" << file << "'" << std::endl;
+        return 1;
+    }
+    printf("%s: Reading input file '%s'... as UCS-2...\n", module, file);
+    std::ofstream ofs;
+    /* creat output file */
+    ofs.open(out_file, std::ios::trunc);
+    if (!ofs) {
+        std::cerr << "Error: FAILED to open out file '" << out_file << "'" << std::endl;
+        ifs.close();
+        return 1;
+    }
+    printf("%s: Writting output file '%s' as UTF-8...\n", module, out_file);
+    while (std::getline(ifs, str)) {
+        //rtrim(str); // FAILED
+        trim_wCR(str);  /* seems to work... */
+        len = str.length();
+        if (lines == 0) {
+            /* check and remove utf-16 BOM from wstring */
+            char c1 = (char)str[0];
+            char c2 = 0;
+            if (len > 1)
+                c2 = (char)str[1];
+            /* is it the BOM */
+            if (((c1 & 0xff) == 0xfe) && ((c2 & 0xff) == 0xff)) {
+                if (len >= 2) {
+                    str2 = str.substr(2);
+                    len = str2.length();
+                    if (len == 0)
+                        continue;
+                    str = str2; // copy the balance
+                }
+                else {
+                    continue;
+                }
+            }
+
+        }
+        lines++;
+        std::string s = ucs2ToUtf8(str);
+        out_len += s.length();
+        ofs << s;
+        ofs << std::endl;
+#ifdef WIN32
+        out_len += 2;
+#else
+        out_len++;
+#endif
+    }
+
+    printf("%s: Read, converted and wrote %u lines, %u bytes... (%u)\n", module, lines, (int)out_len, (int)ofs.tellp());
+    ifs.close();
+    ofs.close();
+    return iret;
+}
+
+
 // main() OS entry
 int main( int argc, char **argv )
 {
@@ -159,8 +257,10 @@ int main( int argc, char **argv )
             iret = 0;
         return iret;
     }
-
-    iret = process_in_file(usr_input); // actions of app
+    if (reverse)
+        iret = process_in_file_UCS2(usr_input); // actions of app
+    else
+        iret = process_in_file_UTF8(usr_input); // actions of app
 
     return iret;
 }
