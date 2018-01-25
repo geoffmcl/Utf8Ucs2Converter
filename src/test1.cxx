4e039a3909e5ff6b59fea337626711a7b7f3a91f
Geoff McLane <ubuntu@geoffair.info>
2018-01-25 20:50:38 +0100

    Add a simple test1.cxx file

---
+++ src/test1.cxx
@@ -0,0 +1,48 @@
+/*\
+ * test1.cxx
+ *
+ * Copyright (c) 2017 - Geoff R. McLane
+ * Licence: GNU GPL version 2
+ *
+ * from : http://en.cppreference.com/w/cpp/locale/codecvt_utf16
+ *
+\*/
+
+#include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <codecvt>
+// other includes
+
+static const char *module = "test1";
+
+static const char* test = "temptest.txt";
+
+void prepare_file()
+{
+    // UTF-16le data (if host system is little-endian)
+    char16_t utf16le[4] ={0x007a,          // latin small letter 'z' U+007a
+                          0x6c34,          // CJK ideograph "water"  U+6c34
+                          0xd834, 0xdd0b}; // musical sign segno U+1d10b
+    // store in a file
+    std::ofstream fout(test);
+    fout.write( reinterpret_cast<char*>(utf16le), sizeof utf16le);
+    fout.close();
+}
+ 
+int main()
+{
+    prepare_file();
+    // open as a byte stream
+    std::wifstream fin(test, std::ios::binary);
+    // apply facet
+    fin.imbue(std::locale(fin.getloc(),
+       new std::codecvt_utf16<wchar_t, 0x10ffff, std::little_endian>));
+ 
+    for (wchar_t c; fin.get(c); )
+            std::cout << std::showbase << std::hex << c << '\n';
+}
+
+// eof = test1.cxx
